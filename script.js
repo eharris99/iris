2867b6e6be69baf0209fad918016337f492c8ffe
elise9876 <eharris99@yahoo.com>
2016-04-12 20:59:05 +0000

    more colors and lis

--- script.js
+++ script.js
@@ -8,14 +8,6 @@ $(document).ready(function(){
 //   $(".nav").find(".active").removeClass("active");
 //   $(this).parent().addClass("active");
 // });
-function sendMail() {
-    var link = "mailto:irisfans@gmail.com"
-             + "&subject=" + escape("I like Iris too")
-             + "&body=" + escape(document.getElementById('myText').value)
-    ;
-
-    window.location.href = link;
-}
 
 
 
@@ -41,4 +33,43 @@ function getPixels(img)
 }
 
 #3A9E1E*/
+
+/* blog plugin js*/
+
+$('#blogcontainer').pinto();
+$('#blogcontainer').pinto({
+
+// a block identification key
+itemSelector: "> div", 
+
+// a class of items that will be skipped and not layouted
+itemSkipClass: "pinto-skip", 
+
+// the width of one grid block in pixels
+itemWidth: 220, 
+
+// the width spacing between blocks in pixels
+gapX: 10, 
+
+// the height spacing between blocks in pixels
+gapY: 10, 
+
+// a blocks alignment ("left", "right", "center")
+align: "left", 
+
+// adjust the block width to create optimal layout based on container size
+fitWidth: true, 
+
+// up<a href="http://www.jqueryscript.net/time-clock/">date</a> layout after browser is resized
+autoResize: true, 
+
+// time in milliseconds between browser resize and layout update
+resizeDelay: 50, 
+
+// fire after item layout complete
+onItemLayout: function($item, column, position) {}, 
+  
+});
+
+/* end of blog js */
 
