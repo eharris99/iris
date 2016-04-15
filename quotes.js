4ac249ee005c311779a83a1bceeed28e712b19c2
elise9876 <eharris99@yahoo.com>
2016-04-15 00:35:15 +0000

    quote generator

---
+++ quotes.js
@@ -0,0 +1,10 @@
+// JavaScript File
+
+function Quote() {
+	
+	
+	
+	var irisquotes =["When you don't dress like everyone else, you don't have to think like everyone else.", "If you make a mistake in what you put together, the fashion police aren not going to haul you into jail.", "I don't see anything so wrong with a wrinkle. It's kind of a badge of courage", "Fashion you can buy, but style you possess."];
+	var randomirisquote = irisquotes[Math.round(Math.random()*(irisquotes.length-1))];
+	document.getElementById('quotes').innerHTML = '<div>' + " " +  randomirisquote + " "  + '</div>';
+}
