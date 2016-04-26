de2e4c22a72f1ec1da1340505a70f586de4f0d92
elise9876 <eharris99@yahoo.com>
2016-04-26 20:24:36 +0000

    game fixes

--- js/quotes.js
+++ js/quotes.js
@@ -2,9 +2,7 @@
 
 function Quote() {
 	
-	
-	
 	var irisquotes =["When you don't dress like everyone else, you don't have to think like everyone else.", "If you make a mistake in what you put together, the fashion police are not going to haul you into jail.", "I don't see anything so wrong with a wrinkle. It's kind of a badge of courage", "Fashion you can buy, but style you possess.", "To lead the good life in New York, the two most important things for a woman are a chauffeur and a fur-lined raincoat.", "I don't expect to find inspiration. It just sort of comes.", "If your hair is done properly and you are wearing good shoes, you can get away with anything.", "There's no how-to roadmap to style. It's about self-expression and, above all, attitude.", "I don't dress to be stared at. I dress for myself.", "If you're not interested, you're not interesting.", "You have to try it. You only have one trip, you've got to remember that.","You don't find out who you are unless you work at it.", "More is more and less is a bore","I think you have to be true to yourself, and not try to be six different people because that’s the look that’s on-trend now","Another mad outfit","I'm not pretty and I'll never be pretty but it doesn't matter. I have something much better, I have style.","It's better to be happy than well-dressed", "I hate it when it looks put-together", "Color can raise the dead"];
-	var randomirisquote = irisquotes[Math.round(Math.random()*(irisquotes.length-1))];
+	var randomirisquote = irisquotes[Math.floor(Math.random()*irisquotes.length)];
 	document.getElementById('quotes').innerHTML = '<div>' + " " +  randomirisquote + " "  + '</div>';
 }
--- js/racing2.js
+++ js/racing2.js
@@ -5,7 +5,7 @@ $('#button2').click(function(){
     $('#birthplace').addClass('active');
   var birthplace=prompt("Where was Iris born?");
   birthplace=birthplace.toUpperCase();
-  if (birthplace=="ASTORIA" || "QUEENS")  {
+  if (birthplace=="ASTORIA"|| birthplace=="ASTORIA QUEENS"|| birthplace=="QUEENS")  {
    $('#player1_strip').find('td.active').removeClass('active').next().addClass('active');
    var husband=prompt("What was Iris's husband's first name?");
    husband=husband.toUpperCase();
@@ -17,37 +17,38 @@ $('#button2').click(function(){
    $('#player1_strip').find('td.active').removeClass('active').next().addClass('active');
    var job=prompt("What was Iris's profession?");
    job=job.toUpperCase();
-  if (job=="INTERIOR DECORATOR" ||"INTERIOR DESIGNER")  {
+  if (job=="INTERIOR DECORATOR"|| job=="INTERIOR DESIGNER")  {
    $('#player1_strip').find('td.active').removeClass('active').next().addClass('active');
    var stark=prompt("Who bought Iris and Carl's company?");
    stark=stark.toUpperCase();
-  if (stark=="STARK" || "STARK CARPETING")  {
+  if (stark=="STARK" || stark=="STARK CARPETING" )  {
    $('#player1_strip').find('td.active').removeClass('active').next().addClass('active');
     var whitehouse=prompt("How many White House administrations did Iris work for? Answer with a numeral.");
-  if (whitehouse=="9")  {
+    whitehouse=whitehouse.toUpperCase();
+  if (whitehouse=="9"|| whitehouse=="NINE")  {
    $('#player1_strip').find('td.active').removeClass('active').next().addClass('active');
     alert("Right answer! You won the Borzoi!");
   }
     else {
-   alert("Wrong answer. Click button to start over.");
+   alert("Wrong answer. She worked for 9 administrations. Click button to start over.");
    $('#player1_strip').find('td.active').removeClass('active');
    $('#birthplace').addClass('active');
     }
   }
     else {
-   alert("Wrong answer. Click button to start over.");
+   alert("Wrong answer. Stark bought Old World Weavers. Click button to start over.");
    $('#player1_strip').find('td.active').removeClass('active');
    $('#birthplace').addClass('active');
     }
     }
     else {
-  alert("Wrong answer. Click button to start over.");
+  alert("Wrong answer. She was an interior decorator. Click button to start over.");
   $('#player1_strip').find('td.active').removeClass('active');
   $('#birthplace').addClass('active');
     }
    } 
    else {
-   alert("Wrong answer. Click button to start over.");
+   alert("Wrong answer. She claims caviar was her drug of choice. Click button to start over.");
    $('#player1_strip').find('td.active').removeClass('active');
    $('#birthplace').addClass('active');
     }
@@ -55,14 +56,14 @@ $('#button2').click(function(){
   
     else  {
 
-alert("Wrong answer. Click button to start over.");
+alert("Wrong answer. Her husband's name was Carl. Click button to start over.");
 $('#player1_strip').find('td.active').removeClass('active');
 $('#birthplace').addClass('active');
     }
   }
   else  {
 
-alert("Wrong answer. Click button to start over.");
+alert("Wrong answer. She grew up in Astoria. Click button to start over.");
 $('#player1_strip').find('td.active').removeClass('active');
 $('#birthplace').addClass('active');
     }
